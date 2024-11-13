a829bb3e51824e2db1eb61e17614a31af19699b4
Daniel <danmdob@gmail.com>
2024-11-13 00:54:42 -0600

    add opengraph-image edit home page

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,42 @@
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+
+// Image metadata
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+// Image generation
+export default async function Image() {
+  // Font
+  const interSemiBold = fetch(
+    new URL('./fonts/GeistVF.woff', import.meta.url)
+  ).then((res) => res.arrayBuffer())
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <h1 className="font-bold text-3xl">
+        Hello Full Stack Guild!
+      </h1>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: 'Inter',
+          data: await interSemiBold,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    }
+  )
+}
