d3872270f1176e0630e6ebec9e3a10e6e16c682c
Daniel <danmdob@gmail.com>
2024-11-13 01:10:43 -0600

    final attempt og image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -3,26 +3,21 @@ import { ImageResponse } from 'next/og'
 export const runtime = 'edge'
 
 // Image metadata
+export const alt = 'Hello Full Stack Guild!'
 export const size = {
   width: 1200,
   height: 630,
 }
-
-export const alt = 'About Acme'
 export const contentType = 'image/png'
 
 // Image generation
 export default async function Image() {
   // Font
-  const font = fetch(
-    new URL('./fonts/GeistVF.woff', import.meta.url)
-  ).then((res) => res.arrayBuffer())
-
   return new ImageResponse(
     (
       // ImageResponse JSX element
       <div style={{
-        fontSize: 128,
+        fontSize: 48,
         background: 'white',
         width: '100%',
         height: '100%',
@@ -38,14 +33,6 @@ export default async function Image() {
       // For convenience, we can re-use the exported opengraph-image
       // size config to also set the ImageResponse's width and height.
       ...size,
-      fonts: [
-        {
-          name: 'GeistVF',
-          data: await font,
-          style: 'normal',
-          weight: 400,
-        },
-      ],
     }
   )
 }
